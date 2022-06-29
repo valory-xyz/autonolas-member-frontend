cb7b5f8cd9def40e12f65665d280be9c65df1d93
mohandast52 <mohandast52@gmail.com>
2022-06-29 16:01:21 +0530

    update goerli chain_id & rpc

--- components/Login/Helpers/index.jsx
+++ components/Login/Helpers/index.jsx
@@ -2,8 +2,9 @@ import WalletConnectProvider from '@walletconnect/web3-provider';
 
 export const provider = new WalletConnectProvider({
   rpc: {
-    31337: 'https://chain.staging.autonolas.tech/',
+    // 31337: 'https://chain.staging.autonolas.tech/',
     // 1: 'https://mainnet.infura.io/v3/a5184169a2dd4263b4c164a088353eec',
+    5: 'https://eth-goerli.alchemyapi.io/v2/_SrakDJgKhVl19B6EUYK9LEiyU1auKsL',
   },
   pollingInterval: 8000,
 });
--- util/constants.jsx
+++ util/constants.jsx
@@ -1,6 +1,4 @@
-export const CHAIN_ID = [1, 4, 31337]; // TODO: update
-
-export const SUPPORTED_NETWORKS = [1, 4, 31337]; // TODO: update
+export const CHAIN_ID = [1, 4, 5, 31337]; // TODO: update
 
 export const CONSTANTS = {
   ETH_GETBALANCE: 'eth_getBalance',
