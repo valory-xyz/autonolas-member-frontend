29bded7d3f0bb1ea352bd8832c21ccc3b6db0e4a
mohandast52 <mohandast52@gmail.com>
2022-07-15 15:55:33 +0530

    fix

--- components/Login/helpers.jsx
+++ components/Login/helpers.jsx
@@ -4,11 +4,13 @@ export const providerOptions = {
   walletconnect: {
     package: WalletConnectProvider, // required
     options: {
-      infuraId: process.env.NEXT_PUBLIC_INFURA_ID, // required
+      infuraId: undefined, // required
       rpc: {
+        1: process.env.NEXT_PUBLIC_MAINNET_URL,
         5: process.env.NEXT_PUBLIC_GOERLI_URL,
         31337: process.env.NEXT_PUBLIC_AUTONOLAS_URL,
       },
     },
+    chainId: 1,
   },
 };
