dedd667e9a0d27cfab75b8cc74202f6317546f5b
mohandast52 <mohandast52@gmail.com>
2023-01-26 16:41:44 +0530

    fix

--- components/Home/VeOlas/WriteFunctionality/IncreaseAmount.jsx
+++ components/Home/VeOlas/WriteFunctionality/IncreaseAmount.jsx
@@ -96,7 +96,7 @@ export const IncreaseAmount = () => {
       {account && (
         <>
           {isMappedAmountZero && <CannotIncreaseAlert />}
-          {hasNoOlasBalance && <AlreadyAllAmountLocked />}
+          {(hasNoOlasBalance && !isMappedAmountZero) && <AlreadyAllAmountLocked />}
         </>
       )}
     </>
