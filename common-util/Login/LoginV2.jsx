a42d238bfd66311d467cc15d6a318b77f65269b6
mohandast52 <mohandast52@gmail.com>
2023-10-24 19:29:22 +0530

    init

--- common-util/Login/LoginV2.jsx
+++ common-util/Login/LoginV2.jsx
@@ -1,29 +1,54 @@
 import { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import Web3 from 'web3';
+import PropTypes from 'prop-types';
+import { Grid } from 'antd';
 import { Web3Modal, Web3Button, Web3NetworkSwitch } from '@web3modal/react';
-import { useAccount, useNetwork, useBalance } from 'wagmi';
-import { COLOR } from '@autonolas/frontend-library';
+import {
+  useAccount, useNetwork, useBalance, useDisconnect,
+} from 'wagmi';
+import styled from 'styled-components';
+import { COLOR, MEDIA_QUERY } from '@autonolas/frontend-library';
+
+import { setChainId, setUserBalance } from 'store/setup/actions';
+import {
+  getChainId,
+  getChainIdOrDefaultToMainnet,
+  isAddressProhibited,
+  notifyError,
+} from 'common-util/functions';
 import { projectId, ethereumClient } from './config';
-import { LoginContainer } from './styles';
+
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  line-height: normal;
+  ${MEDIA_QUERY.mobileL} {
+    margin-top: 0.5rem;
+  }
+`;
+
+const { useBreakpoint } = Grid;
 
 export const LoginV2 = ({
   onConnect: onConnectCb,
   onDisconnect: onDisconnectCb,
   theme = 'light',
 }) => {
-  const { address } = useAccount();
+  const dispatch = useDispatch();
+  const { disconnect } = useDisconnect();
   const { chain } = useNetwork();
-  const { data } = useBalance({ address });
 
   const chainId = chain?.id;
-
-  const { connector } = useAccount({
+  const { address, connector } = useAccount({
     onConnect: ({ address: currentAddress }) => {
-      if (onConnectCb) {
+      if (isAddressProhibited(currentAddress)) {
+        disconnect();
+      } else if (onConnectCb) {
         onConnectCb({
           address: address || currentAddress,
-          balance: data?.formatted,
+          balance: null,
           chainId,
         });
       }
@@ -33,55 +58,117 @@ export const LoginV2 = ({
     },
   });
 
-  useEffect(async () => {
-    try {
-      // This is the initial `provider` that is returned when
-      // using web3Modal to connect. Can be MetaMask or WalletConnect.
-      const modalProvider = connector?.options?.getProvider?.()
-        || (await connector?.getProvider?.());
-
-      if (modalProvider) {
-        // We plug the initial `provider` and get back
-        // a Web3Provider. This will add on methods and
-        // event listeners such as `.on()` will be different.
-        const wProvider = new Web3(modalProvider);
-
-        // *******************************************************
-        // ************ setting to the window object! ************
-        // *******************************************************
-        window.MODAL_PROVIDER = modalProvider;
-        window.WEB3_PROVIDER = wProvider;
-
-        if (modalProvider?.on) {
-          // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
-          const handleChainChanged = () => {
-            window.location.reload();
-          };
-
-          modalProvider.on('chainChanged', handleChainChanged);
-
-          // cleanup
-          return () => {
-            if (modalProvider.removeListener) {
-              modalProvider.removeListener('chainChanged', handleChainChanged);
-            }
-          };
-        }
-      }
-
-      return undefined;
-    } catch (error) {
-      console.error(error);
+  // Update the balance
+  const { data: balance } = useBalance({ address });
+  useEffect(() => {
+    if (balance?.formatted) {
+      dispatch(setUserBalance(balance.formatted));
     }
+  }, [balance?.formatted]);
 
-    return undefined;
+  useEffect(() => {
+    // if chainId is undefined, the wallet is not connected & default to mainnet
+    if (chainId === undefined) {
+      /**
+       * wait for 0ms to get the chainId & set it to redux to avoid race condition
+       * and dependent components are loaded once the chainId is set
+       */
+      setTimeout(() => {
+        const tempChainId = getChainId();
+        dispatch(setChainId(tempChainId));
+      }, 0);
+    } else {
+      const tempChainId = getChainIdOrDefaultToMainnet(chainId);
+      dispatch(setChainId(tempChainId));
+    }
+  }, [chainId]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        // This is the initial `provider` that is returned when
+        // using web3Modal to connect. Can be MetaMask or WalletConnect.
+        const modalProvider = connector?.options?.getProvider?.()
+          || (await connector?.getProvider?.());
+
+        if (modalProvider) {
+          // We plug the initial `provider` and get back
+          // a Web3Provider. This will add on methods and
+          // event listeners such as `.on()` will be different.
+          const wProvider = new Web3(modalProvider);
+
+          // *******************************************************
+          // ************ setting to the window object! ************
+          // *******************************************************
+          window.MODAL_PROVIDER = modalProvider;
+          window.WEB3_PROVIDER = wProvider;
+
+          if (modalProvider?.on) {
+            // https://docs.ethers.io/v5/concepts/best-practices/#best-practices--network-changes
+            const handleChainChanged = () => {
+              window.location.reload();
+            };
+
+            modalProvider.on('chainChanged', handleChainChanged);
+
+            // cleanup
+            return () => {
+              if (modalProvider.removeListener) {
+                modalProvider.removeListener(
+                  'chainChanged',
+                  handleChainChanged,
+                );
+              }
+            };
+          }
+        }
+
+        return () => null;
+      } catch (error) {
+        console.error(error);
+        return () => null;
+      }
+    };
+
+    if (connector && !isAddressProhibited(address)) {
+      getData();
+    }
   }, [connector]);
+
+  // Disconnect if the address is prohibited
+  useEffect(() => {
+    if (address && isAddressProhibited(address)) {
+      disconnect();
+
+      // throw an error
+      notifyError(
+        <>
+          Cannot connect – address is on&nbsp;
+          <a
+            rel="noreferrer"
+            href="https://www.treasury.gov/ofac/downloads/sdnlist.pdf"
+            target="_blank"
+          >
+            OFAC SDN list
+          </a>
+        </>,
+      );
+
+      if (onDisconnectCb) onDisconnectCb();
+    }
+  }, [address]);
+
+  const screens = useBreakpoint();
 
   return (
     <LoginContainer>
       <Web3NetworkSwitch />
       &nbsp;&nbsp;
-      <Web3Button balance="show" avatar="hide" />
+      <Web3Button
+        avatar="hide"
+        balance={screens.xs ? 'hide' : 'show'}
+        icon={screens.xs ? 'hide' : 'show'}
+      />
       <Web3Modal
         projectId={projectId}
         ethereumClient={ethereumClient}
