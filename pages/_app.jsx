4d754ea2537151231a903e6dbc7c1d9764a3b597
Oaksprout <oaksproutthetan@gmail.com>
2023-11-20 14:52:09 +0000

    puts site in maintenance mode

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,41 +1,43 @@
 /* eslint-disable jest/require-hook */
-import Head from 'next/head';
+// import Head from 'next/head';
 import { createWrapper } from 'next-redux-wrapper';
 import PropTypes from 'prop-types';
 
-import { WagmiConfig as WagmiConfigProvider } from 'wagmi';
+// import { WagmiConfig as WagmiConfigProvider } from 'wagmi';
 
-import GlobalStyle from 'components/GlobalStyles';
-import Layout from 'components/Layout';
-import { wagmiConfig } from 'common-util/Login/config';
-import { useRouter } from 'next/router';
+// import GlobalStyle from 'components/GlobalStyles';
+// import Layout from 'components/Layout';
+// import { wagmiConfig } from 'common-util/Login/config';
+// import { useRouter } from 'next/router';
 import initStore from '../store';
 
 require('../styles/antd.less');
 
-const MyApp = ({ Component, pageProps }) => {
-  const router = useRouter();
-  const isNotLegal = router.pathname === '/not-legal';
+const MyApp = () => 'Site in maintenance mode.';
 
-  return (
-    <>
-      <GlobalStyle />
-      <Head>
-        <title>Autonolas Member</title>
-        <meta name="title" content="Manage your veOLAS and buOLAS" />
-      </Head>
-      {isNotLegal ? (
-        <Component {...pageProps} />
-      ) : (
-        <WagmiConfigProvider config={wagmiConfig}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </WagmiConfigProvider>
-      )}
-    </>
-  );
-};
+// const MyApp = ({ Component, pageProps }) => {
+//   const router = useRouter();
+//   const isNotLegal = router.pathname === '/not-legal';
+
+//   return (
+//     <>
+//       <GlobalStyle />
+//       <Head>
+//         <title>Autonolas Member</title>
+//         <meta name="title" content="Manage your veOLAS and buOLAS" />
+//       </Head>
+//       {isNotLegal ? (
+//         <Component {...pageProps} />
+//       ) : (
+//         <WagmiConfigProvider config={wagmiConfig}>
+//           <Layout>
+//             <Component {...pageProps} />
+//           </Layout>
+//         </WagmiConfigProvider>
+//       )}
+//     </>
+//   );
+// };
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
   const pageProps = Component.getInitialProps
