17a72b0def94f9df960b3169469582acc5333123
mohandast52 <mohandast52@gmail.com>
2024-03-07 20:22:57 +0530

    add redux toolkit

--- store/index.js
+++ store/index.js
@@ -1,26 +1,14 @@
-import {
-  combineReducers, createStore, applyMiddleware, compose,
-} from 'redux';
-import thunk from 'redux-thunk';
+import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+
 import setup from './setup';
 
 const rootReducer = combineReducers({ setup });
 
-const enhancers = [];
-const middleware = [thunk];
+const makeStore = () => configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV === 'development',
+});
 
-const composeWithDevTools = typeof window === 'object'
-  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  && (process.env.NODE_ENV === 'development')
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-  : compose;
-
-const composedEnhancers = composeWithDevTools(
-  applyMiddleware(...middleware),
-  ...enhancers,
-);
-
-const makeStore = () => createStore(rootReducer, composedEnhancers);
-
-export const wrapper = createWrapper(makeStore, { debug: process.env.NODE_ENV === 'development' });
+export const wrapper = createWrapper(makeStore);
