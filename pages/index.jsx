e104274ad021669eead01acad22c74098c58ea9d
mohandast52 <mohandast52@gmail.com>
2024-03-11 16:45:06 +0530

    importing dynamically

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,8 @@
-import { VeOlas } from 'components/Home/VeOlas';
+import dynamic from 'next/dynamic';
+
+const VeOlas = dynamic(() => import('components/Home/VeOlas'), {
+  ssr: false,
+  loading: () => <p>Loading from client side...</p>,
+});
 
 export default VeOlas;
