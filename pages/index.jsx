d27efc36fbddd2b5cc90978f87b652cf432d23cf
mohandast52 <mohandast52@gmail.com>
2024-03-14 15:38:47 +0530

    remove unused babel configs

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,6 @@ import dynamic from 'next/dynamic';
 
 const VeOlas = dynamic(() => import('components/Home/VeOlas'), {
   ssr: false,
-  loading: () => <p>Loading from client side...</p>,
 });
 
 export default VeOlas;
