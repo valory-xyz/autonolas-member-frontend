232020ff3f58009a330e7c89db8648946557e5cf
Mohan <mohandast52@gmail.com>
2024-03-20 22:07:06 +0530

    feat: audit changes (#58)
    * remove buOlas create
    
    * remoe commented code
    
    * remove approveOlas for buOlas
    
    * MAX_AMOUNT approval removed
    
    * remove SIGNER_ADDRESS and revokeRequest

--- components/Home/BuOlas/contractUtils.jsx
+++ components/Home/BuOlas/contractUtils.jsx
@@ -1,56 +1,5 @@
-import {
-  getContractAddress,
-  getBuolasContract,
-  getOlasContract,
-} from 'common-util/Contracts';
-import { MAX_AMOUNT, parseEther, sendTransaction } from 'common-util/functions';
-
-const SIGNER_ADDRESS = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
-
-export const approveOlasByOwner = ({ account, chainId }) => new Promise((resolve, reject) => {
-  const contract = getOlasContract();
-  const spender = getContractAddress('buOlas', chainId);
-
-  const fn = contract.methods
-    .approve(spender, MAX_AMOUNT)
-    .send({ from: account });
-
-  sendTransaction(fn, account)
-    .then((response) => {
-      resolve(response);
-    })
-    .catch((e) => {
-      window.console.log('Error occured on approving buOLAS by owner');
-      reject(e);
-    });
-});
-
-/**
- * create lock for buOlas
- * NOTE: this is a internal method and won't be exposed or visible to the user
- */
-export const createBuolasLockRequest = ({ account }) => new Promise((resolve, reject) => {
-  const contract = getBuolasContract();
-
-  // creating lock for the signer with 100 ETH & locked for 4 years.
-  // address is fetched when BE is connected locally
-  const values = {
-    signer: SIGNER_ADDRESS,
-    amount: parseEther('100'),
-    numSteps: 4,
-  };
-
-  const fn = contract.methods
-    .createLockFor(values.signer, values.amount, values.numSteps)
-    .send({ from: account });
-
-  sendTransaction(fn, account)
-    .then((response) => resolve(response?.transactionHash))
-    .catch((e) => {
-      window.console.log('Error occured on creating buOlas lock');
-      reject(e);
-    });
-});
+import { getBuolasContract } from 'common-util/Contracts';
+import { sendTransaction } from 'common-util/functions';
 
 /**
  * Withdraw buOlas
@@ -64,24 +13,6 @@ export const withdrawRequest = ({ account }) => new Promise((resolve, reject) =>
     .then((response) => resolve(response?.transactionHash))
     .catch((e) => {
       window.console.log('Error occured on withdrawing buOlas balance');
-      reject(e);
-    });
-});
-
-/**
- * Revoke buOlas from owner accout
- */
-export const revokeRequest = ({ account }) => new Promise((resolve, reject) => {
-  const contract = getBuolasContract();
-
-  const fn = contract.methods
-    .revoke([SIGNER_ADDRESS])
-    .send({ from: account });
-
-  sendTransaction(fn, account)
-    .then((response) => resolve(response?.transactionHash))
-    .catch((e) => {
-      window.console.log('Error occured on revoking buOlas');
       reject(e);
     });
 });
