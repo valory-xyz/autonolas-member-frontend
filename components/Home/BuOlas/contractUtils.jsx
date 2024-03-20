6c3de63f68f62244b5b8b755da934fad2a2aa186
mohandast52 <mohandast52@gmail.com>
2024-03-20 14:44:02 +0530

    remove SIGNER_ADDRESS and revokeRequest

--- components/Home/BuOlas/contractUtils.jsx
+++ components/Home/BuOlas/contractUtils.jsx
@@ -1,8 +1,6 @@
 import { getBuolasContract } from 'common-util/Contracts';
 import { sendTransaction } from 'common-util/functions';
 
-const SIGNER_ADDRESS = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
-
 /**
  * Withdraw buOlas
  */
@@ -15,24 +13,6 @@ export const withdrawRequest = ({ account }) => new Promise((resolve, reject) =>
     .then((response) => resolve(response?.transactionHash))
     .catch((e) => {
       window.console.log('Error occured on withdrawing buOlas balance');
-      reject(e);
-    });
-});
-
-/**
- * Revoke buOlas from owner accout
- */
-export const revokeRequest = ({ account }) => new Promise((resolve, reject) => {
-  const contract = getBuolasContract();
-
-  const fn = contract.methods
-    .revoke([SIGNER_ADDRESS])
-    .send({ from: account });
-
-  sendTransaction(fn, account)
-    .then((response) => resolve(response?.transactionHash))
-    .catch((e) => {
-      window.console.log('Error occured on revoking buOlas');
       reject(e);
     });
 });
